bdcf618775d36233dca57880402b51ef0c3cd1a9
Igor Rocha <igortice@gmail.com>
2018-03-25 17:54:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,10 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports:      [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers:    [],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
